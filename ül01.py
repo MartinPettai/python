8dd74f46cc05f91ba16920cd4331b461d029d619
MartinPettai <73310313+MartinPettai@users.noreply.github.com>
2022-01-31 11:38:05 +0200

    Add files via upload

---
+++ ül01.py
@@ -0,0 +1,47 @@
+#Harjutus 01
+#Martin Pettai
+#IT21
+#31.01.22
+
+
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+print("  @..@")
+print(" (----)")
+print("( \__/ )")
+print('^^ "" ^^')
+
+
+
+#Sisesta ruudu külje pikkus: 10
+#Vastus: ruudu pindala on 100 cm^2
+a=int(input("Sisesta ruudu külje pikkus: "))
+s = a * a
+print("vastus:ruudu pindala on",s,"cm^2")
+'''
+nimi=input("Lisa oma nimi:")
+print ("Tere", nimi+"!") #pluss ei lisa tühikut
+vanus =int(input("lisa oma vanus: "))
+vanuste_vahe = 17-vanus
+print("meie vanuste vahe on:", vanuste_vahe)
+'''
+
+'''
+a=12
+b=16
+a=b
+s=a*b
+p=2*(a+b)
+print("pindala",s,"ruutmeetrit")
+print ("ümbermõõt",p,"meetrit")
+'''
+
+
+
+'''
+print("tere maailm") #Sõne
+print('tere sa ka') #String
+print (2000) #int / täisarv
+print(22.52) #float / komaarv
+print("ütle \"Tere maailm\"") #escape string
+print(type(2000)) #näitab andmetüüpi
+'''
