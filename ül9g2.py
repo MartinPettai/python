cc92949dc68cf00dfb1b7e459287c95cbde7b967
MartinPettai <73310313+MartinPettai@users.noreply.github.com>
2022-02-28 13:56:14 +0200

    Add files via upload

---
+++ ül9g2.py
@@ -0,0 +1,38 @@
+import math
+import datetime
+#martin pettai
+#it21
+#17.02.22
+#ül09
+
+
+
+import datetime
+from datetime import timedelta
+
+kuup = datetime.datetime.now()
+print(kuup.strftime("%d.veebruar %Y"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
